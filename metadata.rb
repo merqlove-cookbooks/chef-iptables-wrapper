31c7126e0a3119a5947ac669b507f1a6c0866f15
Alexander Merkulov <sasha@merqlove.ru>
2015-10-03 14:36:23 +0300

    bump

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'sasha@merqlove.ru'
 license 'all_rights'
 description 'Installs/Configures iptables'
 long_description 'Installs/Configures iptables'
-version '0.0.1'
+version '0.0.2'
 
 depends 'iptables', '>= 0.0.0'
